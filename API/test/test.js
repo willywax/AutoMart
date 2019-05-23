c7b542943e6d619638495b91c59b3cb47d5c7abc
WillyWax <williamtrans2011@gmail.com>
2019-05-23 08:25:57 +0300

    Added Coveralls

--- API/test/test.js
+++ API/test/test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 
-describe('Array', () => {
+const index = require('../index');
+const app = require('../app');
+
+describe('Array', (done) => {
   describe('#indexOf', () => {
-    it('should return -1 if the item is not in array', () => {
+    it('should return -1 if the item is not in array', (done) => {
       assert.equal([1, 2, 3, 4].indexOf(5), -1);
+      done();
     });
+    // done();
   });
 });
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # AutoMart
 [![Build Status](https://travis-ci.org/willywax/AutoMart.svg?branch=develop)](https://travis-ci.org/willywax/AutoMart)
-
 [![Coverage Status](https://coveralls.io/repos/github/willywax/AutoMart/badge.svg?branch=develop)](https://coveralls.io/github/willywax/AutoMart?branch=develop)
 
 
