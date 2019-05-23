85cee877e64c55f363b4688bb4d6db451966a5e6
WillyWax <williamtrans2011@gmail.com>
2019-05-23 08:09:28 +0300

    Removed Coverage folders

--- API/test/test.js
+++ API/test/test.js
@@ -1,8 +1,8 @@
-const assert = require("assert");
+const assert = require('assert');
 
-describe("Array", () => {
-  describe("#indexOf", () => {
-    it("should return -1 if the item is not in array", () => {
+describe('Array', () => {
+  describe('#indexOf', () => {
+    it('should return -1 if the item is not in array', () => {
       assert.equal([1, 2, 3, 4].indexOf(5), -1);
     });
   });
