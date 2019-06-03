12fb60e3e1881aea7f88ac97abff4325a64cd792
WillyWax <williamtrans2011@gmail.com>
2019-06-03 08:01:20 +0300

    Removed Backend Node App

--- API/routers/flags.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const flagController = require('../controllers/flags');
-
-router.post('/', flagController.flagCar);
-
-module.exports = router;
