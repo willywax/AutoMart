83e9eaf6cb35c4d40372b5991eace8f06e406d6a
WillyWax <williamtrans2011@gmail.com>
2019-06-12 14:40:54 +0200

    Updated Preview of cars and orders

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -2,13 +2,21 @@ var previewModal = document.getElementById('previewModal');
 var orderModal = document.getElementById('orderModal');
 
 var previewBtn = document.getElementById('previewBtn');
+
 var cancelBtn = document.getElementById('cancelBtn');
 
 var orderBtn = document.getElementById('orderBtn');
 
-previewBtn.addEventListener('click', function () {
-    previewModal.style.display = "block";
-});
+var orderModalCloseBtn = document.getElementsByClassName('close')[1];
+var previewModalCloseBtn = document.getElementsByClassName('close')[0];
+
+    previewModalCloseBtn.onclick = function () {
+        previewModal.style.display = "none";
+    }
+
+    orderModalCloseBtn.onclick = function(){
+        orderModal.style.display = "none";
+    }
 
 //Order MOdal Pop Up
 orderBtn.addEventListener('click', function () {
@@ -16,17 +24,19 @@ orderBtn.addEventListener('click', function () {
     orderModal.style.display = "block";
 });
 
-var closeBtn = document.getElementsByClassName('close')[0];
 
-closeBtn.onclick = function () {
+function closeModal(){
     previewModal.style.display = "none";
     orderModal.style.display = "none";
 }
 
-cancelBtn.onclick = function () {
-    previewModal.style.display = "none";
-    orderModal.style.display = "none";
+function openPreview(type){
+    if(type === 'preview')
+        previewModal.style.display = "block";
+    if(type === 'order')
+        orderModal.style.display = "block";
 }
+
 
 window.onclick = function (event) {
     if (event.target === previewModal || event.target === orderModal) {
