a06beb540eb7fb6356d91b0cda6afc8c25e3ec97
WillyWax <williamtrans2011@gmail.com>
2019-06-12 09:57:12 +0200

    Updated Preview Page Design and Added Order Car Page

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,23 +1,36 @@
 var previewModal = document.getElementById('previewModal');
+var orderModal = document.getElementById('orderModal');
+
 var previewBtn = document.getElementById('previewBtn');
 var cancelBtn = document.getElementById('cancelBtn');
 
+var orderBtn = document.getElementById('orderBtn');
+
 previewBtn.addEventListener('click', function () {
     previewModal.style.display = "block";
+});
+
+//Order MOdal Pop Up
+orderBtn.addEventListener('click', function () {
+    previewModal.style.display = "none";
+    orderModal.style.display = "block";
 });
 
 var closeBtn = document.getElementsByClassName('close')[0];
 
 closeBtn.onclick = function () {
     previewModal.style.display = "none";
+    orderModal.style.display = "none";
 }
 
 cancelBtn.onclick = function () {
     previewModal.style.display = "none";
+    orderModal.style.display = "none";
 }
 
 window.onclick = function (event) {
-    if (event.target == previewModal) {
+    if (event.target === previewModal || event.target === orderModal) {
         previewModal.style.display = "none";
+        orderModal.style.display = "none";
     }
 }
