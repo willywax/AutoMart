a7a793f3c333ce35b5b95c8e03406ca2f1a461be
WillyWax <williamtrans2011@gmail.com>
2019-06-13 06:54:46 +0200

    [Start #166636076] Created Admin dashboard page and updated ux-flow

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,5 +1,7 @@
 var previewModal = document.getElementById('previewModal');
 var orderModal = document.getElementById('orderModal');
+var flagModal = document.getElementById('flagModal');
+var submitOrderModal = document.getElementById('submitOrderModal');
 
 var previewBtn = document.getElementById('previewBtn');
 
@@ -10,13 +12,13 @@ var orderBtn = document.getElementById('orderBtn');
 var orderModalCloseBtn = document.getElementsByClassName('close')[1];
 var previewModalCloseBtn = document.getElementsByClassName('close')[0];
 
-    previewModalCloseBtn.onclick = function () {
-        previewModal.style.display = "none";
-    }
+previewModalCloseBtn.onclick = function () {
+    previewModal.style.display = "none";
+}
 
-    orderModalCloseBtn.onclick = function(){
-        orderModal.style.display = "none";
-    }
+orderModalCloseBtn.onclick = function () {
+    orderModal.style.display = "none";
+}
 
 //Order MOdal Pop Up
 orderBtn.addEventListener('click', function () {
@@ -24,18 +26,46 @@ orderBtn.addEventListener('click', function () {
     orderModal.style.display = "block";
 });
 
+// Function to close Modal
+function closeModal(type) {
+    //Handling optional parameter
+    type = typeof type=='undefined' ? 'none' : type;
+    if (type === 'preview')
+        previewModal.style.display = "none";
+    else if (type === 'order')
+        orderModal.style.display = "none";
+    else if (type === 'flag')
+        flagModal.style.display = "none";
+    else if (type === 'submitOrder')
+        submitOrderModal.style.display = "none";
+    else {
+        previewModal.style.display = "none";
+        orderModal.style.display = "none";
+        flagModal.style.display = "none";
+        submitOrderModal.style.display = "none";
+    }
 
-function closeModal(){
-    previewModal.style.display = "none";
-    orderModal.style.display = "none";
 }
 
-function openPreview(type){
-    if(type === 'preview')
+//Function to Open ANy Modal
+function openPreview(type) {
+    //  alert('Clicker');
+    if (type === 'preview')
         previewModal.style.display = "block";
-    if(type === 'order')
+    if (type === 'order')
         orderModal.style.display = "block";
+    if (type === 'flag')
+        flagModal.style.display = "block";
+    if (type === 'submitOrder')
+        submitOrderModal.style.display = "block";
 }
+
+//Confrimation Alert
+function alertBox(text) {
+    alert(text);
+    closeModal();
+}
+
 
 
 window.onclick = function (event) {
