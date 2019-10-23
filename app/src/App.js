27f14dabe0fe756a2f16cd971d8ed46134d92fd6
willywax <williamtrans2011@gmail.com>
2019-10-23 19:22:43 +0200

    change landing page to react

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline, Container } from "@material-ui/core";
 import Layout from './layout';
+import './styles/styles.scss';
 
 class App extends React.Component {
     render() {
